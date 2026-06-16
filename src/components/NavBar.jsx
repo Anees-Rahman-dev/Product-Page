fabad6bba4023a5b9fabb7afaad6c1a65001fce6
Anees-Rahman-dev <aneesrahman0101>
2026-06-16 12:27:20 +0530

    added hover to navbar buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ function NavBar() {
   return (
     <nav className='bg-transparent absolute top-0 left-0 w-full z-50 shadow-lg shadow-gray-700 h-16 flex justify-center items-center '>
         <div className='flex justify-around w-[500px] gap-20 text-white '>
-            <button className='cursor-pointer quicksand'>Explore</button>
-            <button className='cursor-pointer quicksand'>About</button>
-            <button className='cursor-pointer quicksand'>Login</button>
+            <button className='cursor-pointer quicksand hover:scale-110 transition-transform duration-500 ease-in-out'>Explore</button>
+            <button className='cursor-pointer quicksand hover:scale-110 transition-transform duration-500 ease-in-out'>About</button>
+            <button className='cursor-pointer quicksand hover:scale-110 transition-transform duration-500 ease-in-out'>Login</button>
         </div>
     </nav>
   )
