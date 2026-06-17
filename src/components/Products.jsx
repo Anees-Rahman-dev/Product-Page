bd9bbe104144b5dbf8b8fd89b8047716c27bf050
Anees-Rahman-dev <aneesrahman0101>
2026-06-17 12:45:00 +0530

    after making the image as card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,9 +4,9 @@ export default function Products() {
   return (
     <div className='bg-black min-h-[500px] w-full '>
        <div className='flex flex-col md:flex-row sm:flex-row items-center justify-center min-h-[400px] w-full gap-20 '>
-        <img src="/image/katana1.jpeg" alt="" className='h-[300px] md:h-[400px] w-auto object-contain ' />
-        <img src="/image/katana2.jpeg" alt="" className='h-[300px] md:h-[400px] w-auto object-contain'/>
-        <img src="/image/katana3.webp" alt="" className='h-[300px] md:h-[340px] w-auto object-contain' />
+        <img src="/image/katana1.jpeg" alt="" className='h-[300px] md:h-[400px] w-auto object-contain rounded-lg shadow-xl shadow-gray-600 hover:scale-105 transition-transform duration-300 ease-in-out ' />
+        <img src="/image/katana2.jpeg" alt="" className='h-[300px] md:h-[400px] w-auto object-contain rounded-xl shadow-xl shadow-gray-600 hover:scale-105 transition-transform duration-300 ease-in-out'/>
+        <img src="/image/katana3.jpeg" alt="" className='h-[300px] md:h-[400px] w-auto object-contain rounded-lg shadow-xl shadow-gray-600 hover:scale-105 transition-transform duration-300 ease-in-out' />
         </div>
     </div>
   )
