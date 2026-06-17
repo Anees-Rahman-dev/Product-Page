1aab8f6e4a88eb7b7f51e89f0381bb58f3769356
Anees-Rahman-dev <aneesrahman0101>
2026-06-17 10:50:06 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,30 @@ import React from 'react'
 export default function 
 () {
   return (
-    <div className='bg-gray-900 min-h-[400px] w-screen'>
-        
+    <div className='bg-gray-900 min-h-[400px] w-full'>
+     <div className='bg-black min-h-[400px] flex justify-center md:flex-row items-start gap-10 md:gap-24 px-6 md:px-12  py-10 text-white  '>
+         <div className='h-full py-4 px-5  flex flex-col gap-4'>
+          <h1 className='quicksand text-xl font-semibold'>Main Menu</h1>
+          <h3 className='quicksand'>Other Swrords</h3>
+          <h3 className='quicksand'>Antique</h3>
+          <h3 className='quicksand'>Accessories</h3>
+          <h3 className='quicksand'>Samurai Armor</h3>
+        </div>
+
+        <div className='h-full py-4 px-5  flex flex-col gap-4'>
+          <h1 className='quicksand text-xl font-semibold'>Info</h1>
+          <h3 className='quicksand'>Privacy Policy</h3>
+          <h3 className='quicksand'>Shipping</h3>
+          <h3 className='quicksand'>Terms And Service</h3>
+        </div>
+
+        <div className='h-full py-4 px-5  flex flex-col gap-4'>
+          <h1 className='quicksand text-xl font-semibold'>Contact Us</h1>
+          
+          <h3 className='quicksand'>Available 24/7</h3>
+          <h3 className='quicksand'>Customer Service</h3>
+          </div>
+     </div>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,26 +11,26 @@ export default function Hero() {
         })
     }, [])
     return (
-        <div className="relative h-screen w-screen">
+        <div className="relative h-[70vh] md:[85vh] lg:h-screen w-full overflow-hidden">
             <video
                 src="/video/katana.mp4"
                 autoPlay
                 loop
                 muted
-                className="w-full h-full object-cover "
+                className="absolute inset-0 w-full h-full object-cover "
             />
 
-            <div className="absolute inset-0 flex flex-col justify-center px-16 text-white">
+            <div className="relative z-10 flex h-full min-h-screen flex-col justify-center px-6 md:px-16 text-white">
                 <h1
                     data-aos="fade-up"
-                    className="text-6xl font-bold bg-gradient-to-r from-gray-300 via-black-300 to-black-800 bg-clip-text text-transparent">
+                    className="text-4xl md:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-gray-300 via-black-300 to-black-800 bg-clip-text text-transparent">
                     Authentic <span className='bg-gradient-to-r from-gray-300 via-black-300 to-black-800 bg-clip-text text-transparent'> Katana</span>
                 </h1>
 
                 <p
                     data-aos="fade-up"
                     data-aos-delay="300"
-                    className="mt-4 text-xl quicksand">
+                    className="mt-4 text-lg md:text-xl quicksand">
                     Handcrafted Japanese Excellence
                 </p>
             </div>
